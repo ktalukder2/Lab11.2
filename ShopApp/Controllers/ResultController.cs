4ef3009e517181a87825d690dcfa3397db0f8484
Talukder <kutubtalukder@quickenloans.com>
2021-05-17 09:22:37 -0400

    first commit

---
+++ ShopApp/Controllers/ResultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using ShopApp.Models;
+
+namespace ShopApp.Controllers
+{
+    public class ResultController : Controller
+    {
+        [HttpGet]
+        public IActionResult UserRegister(RegisterModel registerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FirstName = registerModel.FirstName;
+                return View();
+            }
+            return View();
+            
+        }
+    }
+}
---
+++ ShopApp/Models/RegisterModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.Models
+{
+    public class RegisterModel
+    {
+        [Display(Name ="First Name")]
+        [Required]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        [Display(Name = "Email")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Display(Name = "Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
